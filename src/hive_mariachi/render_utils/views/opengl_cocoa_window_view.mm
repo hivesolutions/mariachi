65ce71f809713de0df90711acd48099f70ecdd44
joamag <joamag@hive.pt>
2016-02-11 19:51:38 +0000

    new encoding values

--- src/hive_mariachi/render_utils/views/opengl_cocoa_window_view.mm
+++ src/hive_mariachi/render_utils/views/opengl_cocoa_window_view.mm
@@ -16,7 +16,7 @@
 // You should have received a copy of the Apache License along with
 // Hive Mariachi Engine. If not, see <http://www.apache.org/licenses/>.
 
-// __author__    = Jo„o Magalh„es <joamag@hive.pt>
+// __author__    = Jo√£o Magalh√£es <joamag@hive.pt>
 // __version__   = 1.0.0
 // __revision__  = $LastChangedRevision$
 // __date__      = $LastChangedDate$
